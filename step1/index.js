24867caaa1f6f83699e558ad786be9825ecfbdf1
wukai <1183323265@qq.com>
2016-09-15 12:28:26 +0800

    first init

---
+++ step1/index.js
@@ -0,0 +1,3 @@
+var a = require('./a.js');
+console.log('This is a javascript file for test.');
+console.log('a = ', a);
---
+++ step1/webpack.config.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+}
